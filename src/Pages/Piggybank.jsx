15a8ab924e4576a3a8ba7a3e5c1a198b5ec01405
opadeji <opayinkaayodejis@gmail.com>
2025-07-07 00:17:19 +0100

    omoooo

--- src/Pages/Piggybank.jsx
+++ src/Pages/Piggybank.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PiggybankHero from "../Component/block/piggybank/PiggybankHero";
 import PiggyBnkCard from "../Component/block/piggybank/PiggyBnkCard";
 import Pigstart from "../Component/block/piggybank/Pigstart";
-import Pigtest from "../Component/block/piggybank/pigtest";
+import Pigtest from "../Component/block/piggybank/Pigtest";
 
 const Piggybank = () => {
   return (
--- src/Pages/Safelock.jsx
+++ src/Pages/Safelock.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SafelockHero from "../Component/block/safelock/SafelockHero";
-import Safecard from "../Component/block/safelock/safecard";
+import Safecard from "../Component/block/safelock/Safecard";
 import Tempt from "../Component/block/safelock/Tempt";
 import Safetest from "../Component/block/safelock/Safetest";
 
